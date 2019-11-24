6f9652f1c173c50867acf2ae2759daee44bd0533
hang <765926897@qq.com>
2019-11-24 22:19:56 +0800

    完成品牌查询的功能

---
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/BrandController.java
@@ -0,0 +1,33 @@
+package com.leyou.item.controller;
+
+import com.leyou.common.pojo.PageResult;
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("brand")
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    public ResponseEntity<PageResult<Brand>> queryBrandByPage(
+            @RequestParam(value = "key",required = false) String key,
+            @RequestParam(value = "page",defaultValue = "1") Integer page,
+            @RequestParam(value = "rows",defaultValue = "5") Integer rows,
+            @RequestParam(value = "sortby",required = false) String sortBy,
+            @RequestParam(value = "desc",required = false) Boolean desc
+    ){
+        PageResult<Brand> result = this.brandService.queryBrandsByPage(key,page,rows,sortBy,desc);
+        if (CollectionUtils.isEmpty(result.getItems())){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(result);
+    }
+}
