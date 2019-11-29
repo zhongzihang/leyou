7ba19d9296918f248f3f03515f3998dbcce2de76
hang <765926897@qq.com>
2019-11-29 16:55:13 +0800

    基本搜索的页面接口

--- leyou-search/src/main/java/com/leyou/search/service/SearchService.java
+++ leyou-search/src/main/java/com/leyou/search/service/SearchService.java
@@ -2,7 +2,10 @@ package com.leyou.search.service;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leyou.common.pojo.PageResult;
 import com.leyou.item.pojo.*;
+import com.leyou.search.pojo.SearchRequest;
+import com.leyou.search.GoodsReponsitory;
 import com.leyou.search.client.BrandClient;
 import com.leyou.search.client.CategoryClient;
 import com.leyou.search.client.GoodsClient;
@@ -11,7 +14,13 @@ import com.leyou.search.pojo.Goods;
 import org.apache.commons.lang.StringUtils;
 
 import org.apache.commons.lang.math.NumberUtils;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -19,6 +28,8 @@ import java.util.*;
 
 @Service
 public class SearchService {
+    @Autowired
+    private GoodsReponsitory goodsReponsitory;
     @Autowired
     private BrandClient brandClient;
 
@@ -130,6 +141,47 @@ public class SearchService {
             }
         }
         return result;
+    }
+
+
+    /**
+     *
+     * @param request
+     * @return
+     */
+    public PageResult<Goods> search(SearchRequest request) {
+        String key = request.getKey();
+        // 判断是否有搜索条件，如果没有，直接返回null。不允许搜索全部商品
+        if (StringUtils.isBlank(key)) {
+            return null;
+        }
+
+
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+        // 1、对key进行全文检索查询
+        queryBuilder.withQuery(QueryBuilders.matchQuery("all", key).operator(Operator.AND));
+
+        // 2、通过sourceFilter设置返回的结果字段,我们只需要id、skus、subTitle
+        queryBuilder.withSourceFilter(new FetchSourceFilter(
+                new String[]{"id","skus","subTitle"}, null));
+
+        // 3、分页
+        // 准备分页参数
+        int page = request.getPage();
+        int size = request.getSize();
+        queryBuilder.withPageable(PageRequest.of(page - 1, size));
+
+        // 4、查询，获取结果
+        Page<Goods> pageInfo = this.goodsReponsitory.search(queryBuilder.build());
+
+        // 封装结果并返回
+        // 5.1 总条数
+        Long total = pageInfo.getTotalElements();
+        // 5.2 总页数
+        int totalPage = (total.intValue() + size - 1) / size;
+        return new PageResult<>(total, totalPage, pageInfo.getContent());
     }
 
 }
---
+++ leyou-search/src/test/java/com/leyou/search/client/createIndex.java
@@ -0,0 +1,4 @@
+package com.leyou.search.client;
+
+public class createIndex {
+}
